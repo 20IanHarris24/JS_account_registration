b09ae8e1a882fd5953cbbed2d18781a03160d622
20IanHarris24 <ian_harris@ymail.com>
2024-07-12 16:27:06 +0200

    added password check and interactive buttons. improved styling.

--- index.js
+++ index.js
@@ -2,25 +2,30 @@
 const createAccount = document.querySelector(".acc-input");
 const elementInput = createAccount.querySelectorAll(".input"); //for accessing all element of class input. 
 const elementLabel = createAccount.querySelectorAll(".label"); //for accessing all element of class label.
-const btnPress = createAccount.querySelectorAll(".btn-press"); 
+const btnPress = createAccount.querySelector(".btn-press"); 
 
-
+const fullName = createAccount.querySelector(".name-input");
+const userName = createAccount.querySelector(".uname-input");
+const email = createAccount.querySelector(".email-input");
+const password = createAccount.querySelector(".pwd-input");
+const confirmedPassword = createAccount.querySelector(".pwd-confirm");
 
 
 elementInput.forEach((input)=>{
+    
     input.addEventListener("mouseenter",()=>{
     input.classList.add("input-highlight");
     });
 });
 elementInput.forEach((input)=>{
+    
     input.addEventListener("mouseleave",()=>{
     input.classList.remove("input-highlight");
     });
 });
 
-
-//console.log(elementLabel);
 elementLabel.forEach((label)=>{
+  
   label.addEventListener("click",()=>{
     let input = label.firstElementChild;
     highlight(label, input);
@@ -28,146 +33,124 @@ elementLabel.forEach((label)=>{
 });
 
 elementLabel.forEach((label)=>{
-  label.addEventListener("dblclick",()=>{
+  
+  label.addEventListener("mouseleave",()=>{
     let input = label.firstElementChild;
     unlight(label, input);
     });
 });
 
+password.addEventListener("input",()=>{                     //Checking for password input.
+  let pword = password.value.trim();
+ 
+  if(pword.length > 8)
+  {
 
+     password.classList.remove("pwd-is-invalid");
+    password.classList.add("pwd-is-valid");                    //Styling for an invalid input.
+  } else{
 
+        password.classList.add("pwd-is-invalid");               //Styling for an valid input.
+        }
 
-
-console.log(elementLabel);
-//console.log(btnPress);
-
-
-elementLabel.forEach((label)=>{
-    let button = label.firstElementChild;
-    console.log(button);
+  //console.log("Password",pword);
+});  
+  
+  confirmedPassword.addEventListener("input",()=>{
+    let cPword = confirmedPassword.value.trim();
     
-    btn-press.addEventListener("click",()=>{
-      emptyFields(button);
+        
+    if(cPword === password.value)
+      {
+                
+        confirmedPassword.classList.remove("pwd-is-invalid");
+        confirmedPassword.classList.add("pwd-is-valid");
+        buttonOpen();
+        
+        
+      } else{
+        
+        confirmedPassword.classList.add("pwd-is-invalid");
+        buttonclosed();
+        
+      }
      
-    console.log("reached this part of code");
+     });
     
-   
-    });
+  
+    
+//form submit with check
+
+
+createAccount.addEventListener("submit", (event)=>{
+  event.preventDefault();  //stops the window console default refresh behaviour
+  
+  const formPrint = {      // crete a form object to check for a completed form
+    
+    name: fullName.value,
+    userName: userName.value,
+    email: email.value,
+    password: password.value,
+    confirmPassword: confirmedPassword.value, 
+  }; 
+  
+      buttonconfirm(); 
+      console.log("This is the object data: ",formPrint);
+       
 });
 
-
-
-
-
+ 
+ 
 
 //###### Functions ###
+
+
+
 function highlight(hig1, hig2){
-  hig1.classList.add("label-highlight");
-  hig2.classList.add("label-highlight-rim");
-  hig2.focus();
   
+  hig1.classList.add("label-highlight");
+  hig2.focus();
 }
 
 function unlight(unl1, unl2){
-  unl1.classList.remove("label-highlight");
-  unl2.classList.remove("label-highlight-rim");
-  unl2.blur();
   
+  unl1.classList.remove("label-highlight");
+  unl2.blur();
 }
 
-function emptyFields(btn){
-     let test = btn.value;
-    if (test == null || test ==""){
 
-      alert("Each field must be completed");
-    }
-  }
-
-  function verifyPassword(){
-
-      var pw = document.getElementById("password").value;
-      //check empty password field
-      if(pw == "") {
-         alert("This field needs to be completed");
-         return false;
-      }
-     
-     //minimum password length validation
-      if(pw.length < 8) {
-         document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";
-         return false;
-      }
-    
-    //maximum length of password validation
-      if(pw.length > 15) {
-         document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";
-         return false;
-      } else {
-         alert("Password is correct");
-      }
-    }
-  }
+function buttonOpen(){
+  
+  btnPress.disabled = false;
+  //btnPress.classList.remove("disabled");
+  btnPress.innerText = "Sign up today";
+  btnPress.classList.add("btn-style-formready");
+  btnPress.classList.remove("btn-style-std");
+  btnPress.classList.remove("btn-style-press");
+}
 
 
+function buttonclosed(){
 
+  btnPress.disabled = true;
+  //btnPress.classList.add("disabled");
+  btnPress.innerText = "Sign Up!";
+  btnPress.classList.add("btn-style-std");
+  btnPress.classList.remove("btn-style-formready");
+  btnPress.classList.remove("btn-style-press");
+}
 
+function buttonconfirm(){
 
-// createAccount.addEventListener("submit", (event)=>{
-//     event.preventDefault();
-
-// const name = createAccount.querySelector("#name");
-// const username = createAccount.querySelector("#username");
-// const email = createAccount.querySelector("#email");
-// const password = createAccount.querySelector("#password");
-// const confirmedPassword = createAccount.querySelector("#confirmP");
-
-// createAccount.addEventListener("click",()=>{
-//   if (passwordSubmitted.length >=8) {
-//     password.classList.add("pwd-is-valid");
-      
-//       return;
-//     }
-//     password.classList.remove("pwd-is-valid");
-//     password.classList.add("pwd-is-invalid");
-
-// })
-
-
-// const nameSubmitted = name.value;
-// const usernameSubmitted = username.value;
-// const emailSubmitted = email.value;
-// const passwordSubmitted = password.value;
-// const confirmedPasswordSubmitted = passwordSubmitted.value;
-
-// btnPress.addEventListener("click",()=>{
-//         if(btnPress.classList.contains("btn-style-std"))
-            
-//           {
-//             btnPress.classList.remove("btn-style-std");
-//             btnPress.classList.add("btn-style-press");
-//             btnPress.innerText = "You've signed up!";
-//             return;
-//           }
-            
-//             btnPress.classList.remove("btn-style-press");
-//             btnPress.classList.add("btn-style-std");
-//             btnPress.innerText = "Sign up!";
-
-//             const formSubmit = {
-            
-//                         name: nameSubmitted,
-//                         username: usernameSubmitted,
-//                         email: emailSubmitted,
-//                         password: passwordSubmitted,
-//                         } 
-                        
-                        
-//                         console.log(formSubmit);
- 
-//       });
+  btnPress.disabled = true;
+  //btnPress.classList.add("disabled");
+  btnPress.innerText = "Information submited";
+  btnPress.classList.add("btn-style-press");
+  btnPress.classList.remove("btn-style-std");
+  btnPress.classList.remove("btn-style-formready");
+}
 
 
 
 
 
-// }); 
